1e6a96a92d67f0614e65f7591e60b5f7f1e7095b
jamshaidiqbal <jamshaid1824@gmail.com>
2021-06-24 13:26:02 +0500

    layers

--- constructor/views.py
+++ constructor/views.py
@@ -6,7 +6,8 @@ from django.views import View
 # Create your views here.
 
 class EditorView(View):
-    template_name = 'editor.html'
+    # template_name = 'editor.html'
+    template_name = 'editor_layers.html'
 
     def get(self, request, *args, **kwargs):
         product = ProductDesign.objects.filter(id=3).first()
