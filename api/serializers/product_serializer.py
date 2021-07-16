436cfad2bddb7ee6a6b6ec75b63af470680dfb5f
jamshaidiqbal <jamshaid1824@gmail.com>
2021-07-16 17:57:37 +0500

    base ball jacket serializer

--- api/serializers/product_serializer.py
+++ api/serializers/product_serializer.py
@@ -310,6 +310,144 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'front_view', 'left_view', 'right_view', 'back_view']
 
 
+class BaseBJacketFrontViewSerializer(serializers.ModelSerializer):
+    base_b_jac_front_body = ImageDetailSerializer()
+    base_b_jac_front_collar = ImageDetailSerializer()
+    base_b_jac_front_collar_inner = ImageDetailSerializer()
+    base_b_jac_front_hem = ImageDetailSerializer()
+    base_b_jac_front_hem_strips = ImageDetailSerializer()
+    base_b_jac_front_hem_mid = ImageDetailSerializer()
+    base_b_jac_front_button_body = ImageDetailSerializer()
+    base_b_jac_front_button_hem = ImageDetailSerializer()
+    base_b_jac_left_pocket_front = ImageDetailSerializer()
+    base_b_jac_right_pocket_front = ImageDetailSerializer()
+    base_b_jac_left_sleeve_front = ImageDetailSerializer()
+    base_b_jac_left_cuff_front = ImageDetailSerializer()
+    base_b_jac_left_cuff_front_strips = ImageDetailSerializer()
+    base_b_jac_right_sleeve_front = ImageDetailSerializer()
+    base_b_jac_right_cuff_front = ImageDetailSerializer()
+    base_b_jac_right_cuff_front_strips = ImageDetailSerializer()
+
+    class Meta:
+        model = models.BaseBallJacket_Front
+        fields = ['id', 'name', 'base_b_jac_front_body', 'base_b_jac_front_collar',
+                  'base_b_jac_front_collar_inner', 'base_b_jac_front_hem', 'base_b_jac_front_hem_strips',
+                  'base_b_jac_front_hem_mid', 'base_b_jac_front_button_body', 'base_b_jac_front_button_hem',
+                  'base_b_jac_left_pocket_front', 'base_b_jac_right_pocket_front', 'base_b_jac_left_sleeve_front',
+                  'base_b_jac_left_cuff_front', 'base_b_jac_left_cuff_front_strips', 'base_b_jac_right_sleeve_front',
+                  'base_b_jac_right_cuff_front', 'base_b_jac_right_cuff_front_strips'
+                  ]
+
+
+class BaseBJacketBackViewSerializer(serializers.ModelSerializer):
+    base_b_jac_collar_back = ImageDetailSerializer()
+    base_b_jac_collar_strips_back = ImageDetailSerializer()
+    base_b_jac_body_back = ImageDetailSerializer()
+    base_b_jac_hem_back = ImageDetailSerializer()
+    base_b_jac_hem_strips_back = ImageDetailSerializer()
+    base_b_jac_left_sleeve_back = ImageDetailSerializer()
+    base_b_jac_left_cuff_back = ImageDetailSerializer()
+    base_b_jac_left_cuff_strip_back = ImageDetailSerializer()
+    base_b_jac_right_sleeve_back = ImageDetailSerializer()
+    base_b_jac_right_cuff_back = ImageDetailSerializer()
+    base_b_jac_right_cuff_strip_back = ImageDetailSerializer()
+
+    class Meta:
+        model = models.BaseBallJacket_Back
+
+        fields = ['id', 'name',
+                  'base_b_jac_collar_back',
+                  'base_b_jac_collar_strips_back',
+                  'base_b_jac_body_back',
+                  'base_b_jac_hem_back',
+                  'base_b_jac_hem_strips_back',
+                  'base_b_jac_left_sleeve_back',
+                  'base_b_jac_left_cuff_back',
+                  'base_b_jac_left_cuff_strip_back',
+                  'base_b_jac_right_sleeve_back',
+                  'base_b_jac_right_cuff_back',
+                  'base_b_jac_right_cuff_strip_back',
+                  ]
+
+
+class BaseBJacketLeftViewSerializer(serializers.ModelSerializer):
+    base_b_jac_mid_body_left = ImageDetailSerializer()
+    base_b_jac_mid_cuff_left = ImageDetailSerializer()
+    base_b_jac_mid_cuff_strips_left = ImageDetailSerializer()
+    base_b_jac_left_body_left = ImageDetailSerializer()
+    base_b_jac_left_cuff_left = ImageDetailSerializer()
+    base_b_jac_left_cuff_strips_left = ImageDetailSerializer()
+    base_b_jac_right_body_left = ImageDetailSerializer()
+    base_b_jac_right_cuff_left = ImageDetailSerializer()
+    base_b_jac_right_cuff_strips_left = ImageDetailSerializer()
+    base_b_jac_bottom_body_left = ImageDetailSerializer()
+    base_b_jac_bottom_cuff_left = ImageDetailSerializer()
+    base_b_jac_bottom_cuff_strips_left = ImageDetailSerializer()
+
+    class Meta:
+        model = models.BaseBallJacket_Left
+
+        fields = ['id', 'name',
+                  'base_b_jac_mid_body_left',
+                  'base_b_jac_mid_cuff_left',
+                  'base_b_jac_mid_cuff_strips_left',
+                  'base_b_jac_left_body_left',
+                  'base_b_jac_left_cuff_left',
+                  'base_b_jac_left_cuff_strips_left',
+                  'base_b_jac_right_body_left',
+                  'base_b_jac_right_cuff_left',
+                  'base_b_jac_right_cuff_strips_left',
+                  'base_b_jac_bottom_body_left',
+                  'base_b_jac_bottom_cuff_left',
+                  'base_b_jac_bottom_cuff_strips_left',
+                  ]
+
+
+class BaseBJacketRightViewSerializer(serializers.ModelSerializer):
+    base_b_jac_mid_body_right = ImageDetailSerializer()
+    base_b_jac_mid_cuff_right = ImageDetailSerializer()
+    base_b_jac_mid_cuff_strips_right = ImageDetailSerializer()
+    base_b_jac_left_body_right = ImageDetailSerializer()
+    base_b_jac_left_cuff_right = ImageDetailSerializer()
+    base_b_jac_left_cuff_strips_right = ImageDetailSerializer()
+    base_b_jac_right_body_right = ImageDetailSerializer()
+    base_b_jac_right_cuff_right = ImageDetailSerializer()
+    base_b_jac_right_cuff_strips_right = ImageDetailSerializer()
+    base_b_jac_bottom_body_right = ImageDetailSerializer()
+    base_b_jac_bottom_cuff_right = ImageDetailSerializer()
+    base_b_jac_bottom_cuff_strips_right = ImageDetailSerializer()
+
+    class Meta:
+        model = models.BaseBallJacket_Right
+        fields = ['id', 'name',
+                  'base_b_jac_mid_body_right',
+                  'base_b_jac_mid_cuff_right',
+                  'base_b_jac_mid_cuff_strips_right',
+                  'base_b_jac_left_body_right',
+                  'base_b_jac_left_cuff_right',
+                  'base_b_jac_left_cuff_strips_right',
+                  'base_b_jac_right_body_right',
+                  'base_b_jac_right_cuff_right',
+                  'base_b_jac_right_cuff_strips_right',
+                  'base_b_jac_bottom_body_right',
+                  'base_b_jac_bottom_cuff_right',
+                  'base_b_jac_bottom_cuff_strips_right',
+                  ]
+
+
+class BaseBJacDetailSerializer(serializers.ModelSerializer):
+    front_view_base_b_jacket = BaseBJacketFrontViewSerializer()
+    back_view_base_b_jacket = BaseBJacketBackViewSerializer()
+    left_view_base_b_jacket = BaseBJacketLeftViewSerializer()
+    right_view_base_b_jacket = BaseBJacketRightViewSerializer()
+
+    class Meta:
+        model = models.ProductDesign
+        fields = ['id', 'name', 'front_view_base_b_jacket', 'back_view_base_b_jacket', 'left_view_base_b_jacket',
+                  'right_view_base_b_jacket'
+                  ]
+
+
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ProductDesign
