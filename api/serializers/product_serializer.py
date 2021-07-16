950e49b31dbef41aac3b6deddfc1b205ffca9064
jamshaidiqbal <jamshaid1824@gmail.com>
2021-07-16 21:32:29 +0500

    coach jacket

--- api/serializers/product_serializer.py
+++ api/serializers/product_serializer.py
@@ -448,6 +448,119 @@ class BaseBJacDetailSerializer(serializers.ModelSerializer):
                   ]
 
 
+class CoachJacFrontSerializer(serializers.ModelSerializer):
+    coach_jac_body_front = ImageDetailSerializer()
+    coach_jac_collar_front = ImageDetailSerializer()
+    coach_jac_collar_inner_front = ImageDetailSerializer()
+    coach_jac_button_front = ImageDetailSerializer()
+    coach_jac_left_pocket_front = ImageDetailSerializer()
+    coach_jac_right_pocket_front = ImageDetailSerializer()
+    coach_jac_hem_front = ImageDetailSerializer()
+    coach_jac_left_sleeve_front = ImageDetailSerializer()
+    coach_jac_right_sleeve_front = ImageDetailSerializer()
+    coach_jac_left_cuff_front = ImageDetailSerializer()
+    coach_jac_right_cuff_front = ImageDetailSerializer()
+
+    class Meta:
+        model = models.CoachJacket_Front
+        fields = ['id', 'name',
+                  'coach_jac_body_front',
+                  'coach_jac_collar_front',
+                  'coach_jac_collar_inner_front',
+                  'coach_jac_button_front',
+                  'coach_jac_left_pocket_front',
+                  'coach_jac_right_pocket_front',
+                  'coach_jac_hem_front',
+                  'coach_jac_left_sleeve_front',
+                  'coach_jac_right_sleeve_front',
+                  'coach_jac_left_cuff_front',
+                  'coach_jac_right_cuff_front',
+                  ]
+
+
+class CoachJacBackSerializer(serializers.ModelSerializer):
+    coach_jac_body_back = ImageDetailSerializer()
+    coach_jac_collar_back = ImageDetailSerializer()
+    coach_jac_left_sleeve_back = ImageDetailSerializer()
+    coach_jac_right_sleeve_back = ImageDetailSerializer()
+    coach_jac_hem_back = ImageDetailSerializer()
+    coach_jac_left_cuff_back = ImageDetailSerializer()
+    coach_jac_right_cuff_back = ImageDetailSerializer()
+
+    class Meta:
+        model = models.CoachJacket_Back
+        fields = ['id', 'name',
+                  'coach_jac_body_back',
+                  'coach_jac_collar_back',
+                  'coach_jac_left_sleeve_back',
+                  'coach_jac_right_sleeve_back',
+                  'coach_jac_hem_back',
+                  'coach_jac_left_cuff_back',
+                  'coach_jac_right_cuff_back',
+                  ]
+
+
+class CoachJacLeftSerializer(serializers.ModelSerializer):
+    coach_jac_mid_body_left = ImageDetailSerializer()
+    coach_jac_left_body_left = ImageDetailSerializer()
+    coach_jac_right_body_left = ImageDetailSerializer()
+    coach_jac_bottom_body_left = ImageDetailSerializer()
+    coach_jac_mid_cuff_left = ImageDetailSerializer()
+    coach_jac_left_cuff_left = ImageDetailSerializer()
+    coach_jac_right_cuff_left = ImageDetailSerializer()
+    coach_jac_bottom_cuff_left = ImageDetailSerializer()
+
+    class Meta:
+        model = models.CoachJacket_Left
+        fields = ['id', 'name',
+                  'coach_jac_mid_body_left',
+                  'coach_jac_left_body_left',
+                  'coach_jac_right_body_left',
+                  'coach_jac_bottom_body_left',
+                  'coach_jac_mid_cuff_left',
+                  'coach_jac_left_cuff_left',
+                  'coach_jac_right_cuff_left',
+                  'coach_jac_bottom_cuff_left',
+                  ]
+
+
+class CoachJacRightSerializer(serializers.ModelSerializer):
+    coach_jac_mid_body_right = ImageDetailSerializer()
+    coach_jac_left_body_right = ImageDetailSerializer()
+    coach_jac_right_body_right = ImageDetailSerializer()
+    coach_jac_bottom_body_right = ImageDetailSerializer()
+    coach_jac_mid_cuff_right = ImageDetailSerializer()
+    coach_jac_left_cuff_right = ImageDetailSerializer()
+    coach_jac_right_cuff_right = ImageDetailSerializer()
+    coach_jac_bottom_cuff_right = ImageDetailSerializer()
+
+    class Meta:
+        model = models.CoachJacket_Right
+        fields = ['id', 'name',
+                  'coach_jac_mid_body_right',
+                  'coach_jac_left_body_right',
+                  'coach_jac_right_body_right',
+                  'coach_jac_bottom_body_right',
+                  'coach_jac_mid_cuff_right',
+                  'coach_jac_left_cuff_right',
+                  'coach_jac_right_cuff_right',
+                  'coach_jac_bottom_cuff_right',
+                  ]
+
+
+class CoachJacDetailSerializer(serializers.ModelSerializer):
+    front_view_coach_jac = CoachJacFrontSerializer()
+    back_view_base_coach_jac = CoachJacBackSerializer()
+    left_view_coach_jac = CoachJacLeftSerializer()
+    right_view_coach_jac = CoachJacRightSerializer()
+
+    class Meta:
+        model = models.ProductDesign
+        fields = ['id', 'name', 'front_view_coach_jac', 'back_view_base_coach_jac', 'left_view_coach_jac',
+                  'right_view_coach_jac'
+                  ]
+
+
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ProductDesign
