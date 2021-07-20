c4b2b6f1493979532b18bf2a34ad0a2a48157fa5
jamshaidiqbal <jamshaid1824@gmail.com>
2021-07-20 14:52:45 +0500

    cros origin

--- sameditor/settings.py
+++ sameditor/settings.py
@@ -142,5 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://18.210.28.105/'
 ]
