f6a9a7c6cb0493a079eae3f2722ba31d5586b965
jamshaidiqbal <jamshaid1824@gmail.com>
2021-08-11 19:46:43 +0500

    auth

--- constructor/models.py
+++ constructor/models.py
@@ -3,6 +3,29 @@ from django.db import models
 
 # Create your models here.
 
+
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    profile_img = models.ImageField(upload_to='profiles/images', blank=True, null=True)
+    username = models.CharField(blank=True, null=True, max_length=250)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
