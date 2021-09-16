1a0f17fe792dce03f535e10cd05c0ac0558b7755
UmaRSaleem16 <usaleem651@gmail.com>
2021-09-16 15:13:23 +0500

    updated backend

--- pattren/models.py
+++ pattren/models.py
@@ -12,6 +12,13 @@ class LogosCategory(TimeStampedModel):
         return f"{self.name}"
 
 
+class SublimationCategory(TimeStampedModel):
+    name = models.CharField(max_length=250, unique=True, help_text="Sublimation Category Name")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class PrintingMethod(TimeStampedModel):
     name = models.CharField(max_length=250, unique=True, help_text="Printing Method Name")
 
@@ -22,6 +29,16 @@ class PrintingMethod(TimeStampedModel):
 class PresetLogos(TimeStampedModel):
     name = models.CharField(max_length=250, unique=True, help_text="Logo Name")
     category = models.ForeignKey(LogosCategory, on_delete=models.CASCADE, related_name='logos_category')
+    printing_method = models.ManyToManyField(PrintingMethod)
+    image = models.ImageField(upload_to='uploads/preset_logos')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class PresetSublimationPatterns(TimeStampedModel):
+    name = models.CharField(max_length=250, unique=True, help_text="Sublimation Name")
+    category = models.ForeignKey(SublimationCategory, on_delete=models.CASCADE, related_name='sublimation_category')
     printing_method = models.ManyToManyField(PrintingMethod)
     image = models.ImageField(upload_to='uploads/preset_logos')
 
